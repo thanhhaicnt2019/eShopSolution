3ecd9c807c7035ec22dece98c04879e25392ac6f
ThanhHaizz <romaticnolonger98@gmail.com>
2020-10-06 09:24:46 +0700

    Add application

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> create(ProductCreateRequest request);
+
+        Task<int> update(ProductEditRequest request);
+
+        Task<int> delete(int productId);
+
+        Task<List<ProductViewModel>> GetAll();
+
+        Task<PageViewModel<ProductViewModel>> GetAllPaging(string keywork, int pageIndex, int pageSize);
+    }
+}
---
+++ eShopSolution.Application/Dtos/PageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PageViewModel<T>
+    {
+
+        public List<T> Items { set; get; }
+        public int TotalRecord { set; get; }
+    }
+}
